4350cb2675cbac4343cae77ee0198586e4a7061f
Kirill Müller <krlmlr+r@mailbox.org>
2021-03-17 13:55:18 +0100

    Tweak tests

--- tests/testthat/test-create_indiedown_package.R
+++ tests/testthat/test-create_indiedown_package.R
@@ -28,13 +28,23 @@ test_that("create_indiedown_package() fails with meaningful error message", {
 
   expect_error(create_indiedown_package("mydown"), "overwrite")
   expect_error(create_indiedown_package("mydown2"), "file")
+})
+
+test_that("create_indiedown_package() fails for links with meaningful error message", {
+  root <- tempfile("indiedown")
+  dir.create(root)
+
+  withr::local_dir(root)
 
   skip_on_os("windows")
 
+  fs::dir_create("mydown")
   fs::link_create("mydown", "mydown3")
   fs::link_create("mydown-bogus", "mydown4")
   withr::defer(fs::link_delete("mydown-bogus"))
   withr::defer(fs::link_delete("mydown3"))
+  withr::defer(fs::dir_delete("mydown"))
+
   expect_error(create_indiedown_package("mydown3"), "overwrite")
   expect_error(create_indiedown_package("mydown4"), "link")
 })
