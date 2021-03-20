a0203905e403bd2c24c03695f89bb63a07448339
Christoph Sax <christoph.sax@gmail.com>
2021-03-20 17:02:06 +0100

    cd_format_date tweaks

--- inst/mypackage/R/cd_format_date.R
+++ inst/mypackage/R/cd_format_date.R
@@ -6,12 +6,13 @@
 #' @param lang Language, either `"de-DE"`, `"de-CH"` or `"en_US"`
 #' @return Character string, date in local format
 #' @examples
-#' cd_format_date("2012-01-01", "de-CH")
-#' cd_format_date("2012-01-01", "en_US")
+#' cd_format_date("2012-01-01", "de-DE")
+#' cd_format_date("2012-01-01", "en-US")
 #' @export
-cd_format_date <- function(date, lang = default(rmarkdown::metadata$lang, "de_CH")) {
+cd_format_date <- function(date, lang = default(rmarkdown::metadata$lang, "en-US")) {
+  date <- as.Date(date)
   if (lang %in% c("german", "de-DE", "de-CH")) {
-    withr::with_locale(c("LC_TIME" = "de_CH"), format(date, "%e. %B %Y"))
+    withr::with_locale(c("LC_TIME" = "de_DE"), format(date, "%e. %B %Y"))
   } else {
     withr::with_locale(c("LC_TIME" = "en_US"), format(date, "%B %e, %Y"))
   }
