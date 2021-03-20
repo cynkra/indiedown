3ba526618e4c4b7687b73633edba7cf3d94c824b
Christoph Sax <christoph.sax@gmail.com>
2021-03-20 16:58:58 +0100

    revert cd_format_date

--- inst/mypackage/R/cd_format_date.R
+++ inst/mypackage/R/cd_format_date.R
@@ -9,42 +9,10 @@
 #' cd_format_date("2012-01-01", "de-CH")
 #' cd_format_date("2012-01-01", "en_US")
 #' @export
-cd_format_date <- function(date, lang = rmarkdown::metadata$lang) {
-
-  date <- as.Date(date)
-
-  if (is.null(lang)) {
-    lang = "en-US"
-  }
-
-  if (lang %in% c("ngerman", "de-DE", "de-CH")) {
-    withr::with_locale(c("LC_TIME" = find_locale("de_CH")), format(date, "%e. %B %Y"))
+cd_format_date <- function(date, lang = default(rmarkdown::metadata$lang, "de_CH")) {
+  if (lang %in% c("german", "de-DE", "de-CH")) {
+    withr::with_locale(c("LC_TIME" = "de_CH"), format(date, "%e. %B %Y"))
   } else {
-    withr::with_locale(c("LC_TIME" = find_locale("en_US")), format(date, "%B %e, %Y"))
+    withr::with_locale(c("LC_TIME" = "en_US"), format(date, "%B %e, %Y"))
   }
-}
-
-available_locales <- (function() {
-  locales <- NULL
-  function() {
-    if (is.null(locales)) {
-      locales <- system2("locale", "-a", stdout = TRUE)
-    }
-    locales
-  }
-})()
-
-find_locale <- function(main) {
-  locales <- available_locales()
-  utf8 <- grep(paste0(main, ".*utf"), locales, ignore.case = TRUE, value = TRUE)
-  if (length(utf8) > 0) {
-    return(utf8)
-  }
-
-  if (main %in% locales) {
-    warning("Using locale ", main, ", because UTF-8 variant not found.", call. = FALSE)
-    return(main)
-  }
-
-  stop("Can't find locale ", main, call. = FALSE)
 }
